f877f92900735a50e97fb978ed06a7f0e4a8f333
Vilija Mažeikė <vilija.setkute@gmail.com>
2022-05-27 23:27:36 +0300

    no .idea

--- server/server.js
+++ server/server.js
@@ -6,20 +6,6 @@ const http = require("http");
 const server = http.createServer(app);
 const cors = require("cors");
 
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-//     res.setHeader(
-//         "Access-Control-Allow-Methods",
-//         "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//     );
-//     res.setHeader(
-//         "Access-Control-Allow-Headers",
-//         "X-Requested-With,content-type,Origin"
-//     );
-//     res.setHeader("Access-Control-Allow-Credentials", true);
-//     next();
-// });
-
 app.use(cors({origin: true, credentials: true}));
 app.use(express.json());
 app.use("/", mainRoute);
